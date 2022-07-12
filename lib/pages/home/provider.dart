de5e14fa9f4d68e284273d85a1e32b8438ab4cad
Tukhtamurodov-Sardorbek <sardortukhtamurodov02@gmail.com>
2022-07-12 20:59:08 +0500

    🛡 Code Optimized 🛡

---
+++ lib/pages/home/provider.dart
@@ -0,0 +1,22 @@
+// import 'package:flutter/material.dart';
+//
+// class HomeProvider extends ChangeNotifier {
+//   int _currentPageIndex = 0;
+//   bool _showFAB = true;
+//
+//   set currentPageIndex(int index){
+//     if(_currentPageIndex != index){
+//       _currentPageIndex = index;
+//       notifyListeners();
+//     }
+//   }
+//   set showFAB(bool value){
+//     if(_showFAB != value){
+//       _showFAB = value;
+//       notifyListeners();
+//     }
+//   }
+//
+//   int get currentPageIndex => _currentPageIndex;
+//   bool get showFAB => _showFAB;
+// }
